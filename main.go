8c2964b1adaa5e380486c69860674269f027c1a6
Tejasvi T <tejasvi.thota@gmail.com>
2020-08-31 20:25:27 +0100

    Adding test settings

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"net/http"
 )
 
 var (
@@ -17,11 +18,13 @@ func init() {
 	flag.StringVar(&bucket, "bucket", "", "S3 bucket name")
 	flag.StringVar(&accessKey, "accessKey", "", "AWS access key")
 	flag.StringVar(&secretKey, "secretKey", "", "AWS secret key")
-	flag.StringVar(&address, "address", "", "address:port to serve the s3 content")
-	flag.StringVar(&sslCert, "sslCert", "", "Location to SSL certificate")
+	flag.StringVar(&address, "address", "127.0.0.1:3000", "address:port to serve the s3 content")
 }
 
 func main() {
 	flag.Parse()
-	fmt.Println("Hello, I am S4!")
+	fs := http.FileServer(http.Dir("./static_test"))
+
+	log.Println("Server started listening on: ", address)
+	log.Fatal(http.ListenAndServe(address, fs))
 }
